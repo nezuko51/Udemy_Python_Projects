6fb273919b6a2995b544c2c2b2270b264b6cea52
Jessica Kim <jessica.kim.167@my.csun.edu>
2022-12-07 21:45:21 -0800

    Caesar Cipher (final)_updated (12/07/22)

--- Projects/caesar_cipher.py
+++ Projects/caesar_cipher.py
@@ -15,6 +15,23 @@ import sys
 # Global variables
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
 
 #############################################################################################
 # def encrypt(plain_text, shift_amount):
@@ -88,7 +105,7 @@ def caesar(start_text, shift_amount, cipher_direction):
       new_string += new_letter
     else:
       new_string += i
-  print(f"The {cipher_direction}d text is {new_string}")
+  print(f"\nThe {cipher_direction}d text is {new_string}")
 
 ############################################################################################
 # automated loop for continuous use unless specified otherwise
@@ -96,18 +113,20 @@ def caesar(start_text, shift_amount, cipher_direction):
 #       will need to split string, encode/decode two strings, join strings back together when returning or printing final 
 #       encoded/decoded text --> FIXED!! (12/07/22)
 run = True
+print(logo)
+print("\n\nCURRENT PROGRAM ACCOUNTS FOR WHITE SPACE, SPECIAL CHARACTERS, & NUMBERS! TRY OUT A SENTENCE!")
+print("REMEMBER, SHIFT NUMBER MUST BE THE SAME FOR BOTH ENCODING AND DECODING A MESSAGE!\n")
+print("IF SHIFT IS 100 TO ENCODE, IT MUST BE 100 TO DECODE")
+print("BE SURE TO COPY THE ENCODED TEXT TO PASTE FOR DECODING\n")
 while (run):
-  
   direction = input("\nType 'encode' to encrypt, type 'decode' to decrypt:\n")
-  print("\nCURRENT PROGRAM DOES NOT ACCOUNT FOR WHITE SPACE! IT CAN ONLY PROCESS 1 STRING WITH NO SPACES.")
   text = input("\nType your message:\n").lower()
   shift = int(input("\nType the shift number:\n"))
-  print("\REMEMBER, SHIFT NUMBER MUST BE THE SAME FOR BOTH ENCODING AND DECODING A MESSAGE!\n")
 
   caesar(start_text = text, shift_amount = shift, cipher_direction = direction)
  
-  end_code = (input("To end, type 'yes'. To continue, type 'no'\n").lower())
-  if (end_code == "yes"):
+  end_code = (input("\nType 'yes' if you want to go again. Otherwise, type 'no'\n").lower())
+  if (end_code == "no"):
     break
   else:
     continue
